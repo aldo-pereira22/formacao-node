a21eb63109e750ad410da47d804ab830b22cb072
Aldo pereira <aldopereira.gyn@gmail.com>
2022-03-18 17:34:28 -0300

    Exibindo variaveis no EJS

--- inicio/projeto-perguntas-e-respostas/index.js
+++ inicio/projeto-perguntas-e-respostas/index.js
@@ -4,8 +4,16 @@ const app = express()
 // Configurando o EJS para usar o HTML
 app.set('view engine', 'ejs')
 
-app.get("/",(req, res) => {
-    res.render("principal/perfil")
+app.get("/:nome/:lang",(req, res) => {
+
+    let nome =req.params.nome
+    let lang = req.params.lang
+    res.render("index", {
+        nome:nome,
+        lang:lang,
+        empresa:"Empresa do Aldo",
+        inscritos: 8000
+    })
 })
 
 app.listen(8080, () => {
