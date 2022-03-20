88a40769ba35053737ba803a5629b061b9f5efac
Aldo pereira <aldopereira.gyn@gmail.com>
2022-03-20 20:30:59 -0300

    Utilizando o forEache no EJS

--- inicio/projeto-perguntas-e-respostas/index.js
+++ inicio/projeto-perguntas-e-respostas/index.js
@@ -9,13 +9,27 @@ app.get("/:nome/:lang",(req, res) => {
     let nome =req.params.nome
     let lang = req.params.lang
     let exibirMsg = true
+    
+
+    var produtos = [
+        {nome: "Doritos", preco:4.99},
+        {nome: "Coca Cola", preco:8.99},
+        {nome: "Leite de Cabra", preco:20.09},
+        {nome: "Contra Filé", preco:38.99},
+        {nome: "Coxão Mole", preco:42.99},
+        {nome: "RedBull", preco:42.99},
+
+
+    ]
 
     res.render("index", {
         nome:nome,
         lang:lang,
         empresa:"Empresa do Aldo",
         inscritos: 8000,
-        msg:exibirMsg
+        msg:exibirMsg,
+        produtos: produtos
+
     })
 })
 
