d0b16b6197677912b7345fc5996a8023d85f7295
Aldo pereira <aldopereira.gyn@gmail.com>
2022-03-20 21:03:10 -0300

    Instalação do Bootstrap

--- inicio/projeto-perguntas-e-respostas/index.js
+++ inicio/projeto-perguntas-e-respostas/index.js
@@ -5,30 +5,9 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get("/:nome/:lang",(req, res) => {
+app.get("/",(req, res) => {
 
-    let nome =req.params.nome
-    let lang = req.params.lang
-    let exibirMsg = true
-
-    var produtos = [
-        {nome: "Doritos", preco:4.99},
-        {nome: "Coca Cola", preco:8.99},
-        {nome: "Leite de Cabra", preco:20.09},
-        {nome: "Contra Filé", preco:38.99},
-        {nome: "Coxão Mole", preco:42.99},
-        {nome: "RedBull", preco:42.99},
-    ]
-
-    res.render("index", {
-        nome:nome,
-        lang:lang,
-        empresa:"Empresa do Aldo",
-        inscritos: 8000,
-        msg:exibirMsg,
-        produtos: produtos
-
-    })
+    res.render("index")
 })
 
 app.listen(8080, () => {
