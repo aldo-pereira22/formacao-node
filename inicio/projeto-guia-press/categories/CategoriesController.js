88c91aa304c59ea630a0a26cc1cfb454e46040a6
Aldo pereira <aldopereira.gyn@gmail.com>
2022-03-24 12:19:51 -0300

    Atualização de categorias

--- inicio/projeto-guia-press/categories/CategoriesController.js
+++ inicio/projeto-guia-press/categories/CategoriesController.js
@@ -8,43 +8,43 @@ router.get("/admin/categories/new", (req, res) => {
 })
 
 router.post("/categories/save", (req, res) => {
-    let title  = req.body.title
-    if(title != undefined){
+    let title = req.body.title
+    if (title != undefined) {
         Category.create({
             title: title,
             slug: slugify(title)
-        }).then( () => {
+        }).then(() => {
             res.redirect('/admin/categories')
         })
-    }else {
+    } else {
         res.redirect("/admin/categories/new")
     }
 })
 
-router.get("/admin/categories", (req,res) => {
+router.get("/admin/categories", (req, res) => {
     Category.findAll().then(categories => {
 
-        res.render('admin/categories/index', {categories:categories})
+        res.render('admin/categories/index', { categories: categories })
     })
 
 })
 
 router.post("/categories/delete", (req, res) => {
     let id = req.body.id
-    if(id != undefined){
-        if(!isNaN(id)){
+    if (id != undefined) {
+        if (!isNaN(id)) {
 
             Category.destroy({
                 where: {
-                    id:id
+                    id: id
                 }
             }).then(() => {
                 res.redirect("/admin/categories")
             })
-        }else {
+        } else {
             res.redirect("/admin/categories")
         }
-    }else {
+    } else {
         res.redirect("/admin/categories")
     }
 })
@@ -52,20 +52,37 @@ router.post("/categories/delete", (req, res) => {
 router.get("/admin/categories/edit/:id", (req, res) => {
     let id = req.params.id
 
-    if(isNaN(id)){
+    if (isNaN(id)) {
         res.redirect("/admin/categories")
     }
     Category.findByPk(id)
-        .then( category => {
-            if(category != undefined){
-                res.render("admin/categories/edit", {category:category})
-            }else {
+        .then(category => {
+            if (category != undefined) {
+                res.render("admin/categories/edit", { category: category })
+            } else {
                 res.redirect("/admin/categories")
 
             }
-        }).catch( err => {
+        }).catch(err => {
             res.redirect("/admin/categories")
         })
+})
+
+router.post("/categories/update", (req, res) => {
+    let id = req.body.id
+    let title = req.body.title
+    Category.update(
+        { title: title, slug:slugify(title) },
+        {
+            where:
+            {
+                id: id
+            }
+        }
+    ).then( () => {
+        res.redirect("/admin/categories")
+
+    })
 })
 
 module.exports = router
