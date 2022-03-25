0ad9e348cac57cd8da74202d500e8da96f7f7888
Aldo pereira <aldopereira.gyn@gmail.com>
2022-03-25 11:05:38 -0300

    Página de criação de um novo artigo. Rota de criação de um novo artigo

--- inicio/projeto-guia-press/articles/ArticlesController.js
+++ inicio/projeto-guia-press/articles/ArticlesController.js
@@ -1,12 +1,34 @@
 const expres = require('express')
 const router = expres.Router()
+const Category = require('../categories/Category')
+const Article = require('./Article')
+const slugfy = require('slugify')
 
-router.get('/articles', (req, res) => {
+router.get('/admin/articles', (req, res) => {
     res.send("Rota de ARTIGOS")
 })
 
 router.get('/admin/articles/new', (req, res) => {
-    res.render("admin/articles/new")
+    Category.findAll().then(categories => {
+
+        res.render("admin/articles/new", {categories:categories})
+    })
+})
+
+router.post('/articles/save', (req, res) => {
+    let title = req.body.title
+    let body = req.body.body
+    let category = req.body.category
+
+
+    Article.create({
+        title:title,
+        slug:slugfy(title),
+        body:body,
+        categoryId: category
+    }).then( () => {
+        res.redirect('/admin/articles')
+    })
 })
 
 module.exports = router
--- inicio/projeto-guia-press/database/database.js
+++ inicio/projeto-guia-press/database/database.js
@@ -2,8 +2,9 @@ const Sequelize = require('sequelize')
 
 
 const connection = new Sequelize('guiapress','root','root',{
-    host:'172.17.0.4',
-    dialect:'mysql'
+    host:'172.17.0.2',
+    dialect:'mysql',
+    timezone:'-03:00'
 })
 
 module.exports = connection
