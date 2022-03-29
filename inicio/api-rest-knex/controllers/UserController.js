e593eec30e6985822e2d4a1190b11c3801d83cc2
Aldo pereira <aldopereira.gyn@gmail.com>
2022-03-28 21:46:08 -0300

    Cadastro de usuarios

--- inicio/api-rest-knex/controllers/UserController.js
+++ inicio/api-rest-knex/controllers/UserController.js
@@ -1,10 +1,35 @@
+const User = require('../models/User')
 class UserController {
+
+
     async index(req, res) {
 
     }
-    async create(req, res) { 
-        console.log(req.body)
-        res.send("Pegando o corpo da requisição")
+    async create(req, res) {
+        let { email, name, password } = req.body
+
+        if (email == undefined || email == "") {
+            res.status(400)
+            console.log("Não autorizado")
+            res.json({ err: "O E-mail é invalido" })
+            return
+        }
+
+
+        let emailExists = await User.findEmail(email)
+        if(emailExists){
+            res.status(406)
+            res.json({err:"O e-mail ja está cadastrado"})
+            return 
+        }
+        
+        await User.new(email, password, name)
+        res.status(200)
+        console.log("Aprovado")
+        res.send("Cadastrado com sucesso!")
+
+
+
     }
 
 }
