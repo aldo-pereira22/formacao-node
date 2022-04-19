47032618baa2bfdd552a73457b62539d04190df5
Aldo pereira <aldopereira.gyn@gmail.com>
2022-04-19 15:16:20 -0300

    Finalizando validação

--- inicio/validacao_formularios/index.js
+++ inicio/validacao_formularios/index.js
@@ -3,18 +3,22 @@ const express = require('express')
 const flash = require('express-flash')
 const app = express()
 const session = require('express-session')
+const cookieParser = require('cookie-parser')
+
 app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 app.use(flash())
+app.use(cookieParser("123"))
 
 app.use(session({
-    secret: 'keyboard cat',
+    secret: 'keyboardcat',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true }
+
+    cookie: { maxAge: 60000 }
 }))
 
 
@@ -33,20 +37,38 @@ app.post('/form', (req, res) => {
         pontosError = "Vc não pode ter menos de 20 pontos"
     }
 
-    if (nome == undefined || '') {
+    if (nome == undefined || nome == '') {
 
         nomeError = " O nome não pode ser vazio"
     }
 
+    if (nome.length < 4) {
+        nomeError = "O nome é muito pequeno"
+    }
     if (emailError != undefined || pontosError != undefined || nomeError != undefined) {
-        res.send("ERRO NO FORMULÁRIO")
+        req.flash("emailError", emailError)
+        req.flash("pontosErro", pontosError)
+        req.flash("nomeError", nomeError)
+        emailError = (emailError == undefined || emailError.length == 0) ? undefined : emailError
+        pontosError = (pontosError == undefined || pontosError.length == 0) ? undefined : pontosError
+        nomeError = (nomeError == undefined || nomeError.length == 0) ? undefined : nomeError
+
+
+        res.render("index", { emailError, pontosError, nomeError })
     } else {
         res.send("SHOW DE BOLA")
     }
 
 })
 app.get('/', (req, res) => {
-    res.render("index")
+    let emailError = req.flash('emailError')
+    let pontosError = req.flash('pontosError')
+    let nomeError = req.flash('nomeError')
+    let email = req.flash('email')
+    emailError = (emailError == undefined || emailError.length == 0) ? undefined : emailError
+    email = (email == undefined || email.length == 0) ? "" : email
+
+    res.render("index", { emailError, pontosError, nomeError, email: email })
 })
 
 app.listen(5000, (req, res) => {
