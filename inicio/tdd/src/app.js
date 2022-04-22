d63faef82013b4b86afde75e32c6d6b1fd0801f5
Aldo pereira <aldopereira.gyn@gmail.com>
2022-04-21 21:47:20 -0300

    FInalização do modulo de conceitos de TDD

--- inicio/tdd/src/app.js
+++ inicio/tdd/src/app.js
@@ -8,3 +8,11 @@ app.get("/", (req, res) => {
 app.listen(3131, () => {
     console.log("rodando")
 })
+
+app.get("/cor/:pessoa", (req, res) => {
+    console.log("ALDO")
+    let pessoa = req.params.pessoa
+    if (pessoa == "aldo") {
+        res.json("vermelho")
+    }
+})
--- inicio/tdd/test/geral.test.js
+++ inicio/tdd/test/geral.test.js
@@ -4,8 +4,12 @@ let request = supertest("http://localhost:3131")
 test("A aplicação deve responder na porta 3121", async() => {
     // return request.get('/').then(res => expect(res.statusCode).toEqual(200))
 
-    let res = await request.get('/')
+    let res = await request.get('/').then(res => expect(res.statusCode).toEqual(200))
 
-    expect(res.statusCode).toEqual(200)
+})
 
+test("Deve retornar vermelho como cor favorita", (req, res) => {
+    return request.get("/cor/aldo").then(res => {
+        expect(res.body.cor).toEqual("vermelho")
+    })
 })
